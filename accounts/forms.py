b2c82b42fb3873e55e3c64e12618e20937637887
Xabier Díez <xabi@MacBook-Air-de-Xabier.local>
2026-01-26 17:23:13 +0100

    Hospital de referencia al iniciar sesión, email no obligatorio

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,3 @@
-# accounts/forms.py
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
@@ -7,9 +6,15 @@ User = get_user_model()
 
 class SignupForm(UserCreationForm):
     email = forms.EmailField(
-        required=True,
+        required=False, # True para hacerlo obligatorio
         label="Email",
         widget=forms.EmailInput(attrs={"class": "form-control"})
+    )
+
+    hospital_default = forms.CharField(
+        required=False,
+        label="Hospital de referencia",
+        widget=forms.TextInput()
     )
 
     class Meta(UserCreationForm.Meta):
@@ -17,10 +22,15 @@ class SignupForm(UserCreationForm):
         fields = ("username", "email", "password1", "password2")
 
     def clean_email(self):
-        email = self.cleaned_data["email"].strip().lower()
+        # email = self.cleaned_data["email"].strip().lower()
+        email = (self.cleaned_data.get("email") or "").strip().lower()
+
+        # return email
+        if not email:
+            return ""
+
         if User.objects.filter(email__iexact=email).exists():
             raise forms.ValidationError("Ya existe una cuenta con ese email.")
-        return email
 
     def save(self, commit=True):
         user = super().save(commit=False)
--- accounts/views.py
+++ accounts/views.py
@@ -3,15 +3,32 @@ from django.contrib.auth import login
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from .forms import SignupForm
+from users.models import Profile
+from django.db import transaction
 
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect("case_list")
+
     if request.method == "POST":
         form = SignupForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            with transaction.atomic():
+                user = form.save(commit=False)
+                # user.email = form.cleaned_data["email"].strip().lower()
+                email = (form.cleaned_data.get("email") or "").strip().lower()
+                user.email = email  # puede ser ""
+                user.save()
+
+                profile, _ = Profile.objects.get_or_create(user=user)
+                profile.hospital_default = form.cleaned_data["hospital_default"].strip()
+                profile.save()
+
             login(request, user)
-            messages.success(request, "Cuenta creada.")
+            
+            messages.success(request, "Cuenta creada. Bienvenido/a 👋")
             return redirect("case_list")
+
     else:
         form = SignupForm()
 
