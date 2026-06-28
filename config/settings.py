db42f2732479a116fe7814db93781f99dce6af5a
Xabier Díez <xabi@MacBook-Air-de-Xabier.local>
2026-06-28 09:40:49 +0200

    Fix imports y sintaxis PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os dj_database_url
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ if DB_NAME:
         'default': dj_database_url.config(
             default=os.environ.get('DATABASE_URL'),
             conn_max_age=600
-        }
+        )
     }
 else:
     DATABASES = {
